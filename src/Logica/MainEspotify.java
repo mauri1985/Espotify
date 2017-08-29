f66f5acb2acd53a34e6651fae8b3ef52b07c811a
Mauricio <>
2017-08-29 01:54:13 -0300

    //Bajando a tierra

--- src/Logica/MainEspotify.java
+++ src/Logica/MainEspotify.java
@@ -18,12 +18,9 @@ public class MainEspotify {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
+        
                 new MenuPrincipal().setVisible(true);
-            }
-        });
+            
     }
     
 }
